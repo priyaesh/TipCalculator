afb86a6f867de9f30d72520781f947833adea4b4
hemapriya <hemapriyasampath@gmail.com>
2019-06-11 14:47:01 -0400

    version checked

--- jakefile.js
+++ jakefile.js
@@ -14,14 +14,14 @@
         task("version",function(){
             console.log("checking node version:.");
     
-            var packageJson = require("./package.json");
-            var expectedVersion = "v"+packageJson.engines.node;
-            var actualVersion = process.version;
+             var packageJson = require("./package.json");
+             var expectedVersion = "v"+packageJson.engines.node;
+             var actualVersion = process.version;
            
               if(semver.neq(expectedVersion,actualVersion)){
                   fail("Incorrect node version : Expected"+expectedVersion +"but was actual"+actualVersion);
               }
     
-        });
+         });
 
 }());
